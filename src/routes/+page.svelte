696ad3b77185a4c565e3ff159d55aef520ad39da
Ethan Burd <54852139+burdbrains@users.noreply.github.com>
2024-11-01 17:57:52 -0500

    Added nextup logo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,8 @@
     import BidModal from '$lib/components/BidModal.svelte';
     import type { Song } from '$lib/types/song';
 
+    import nextup from '$lib/assets/nextup.png';
+
     let songs: Song[] = [];
     let nextUpSong: Song | null = null;
     let unsubscribeQueue: (() => void) | null = null;
@@ -85,7 +87,10 @@
         <svg class="location-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zM12 11.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="currentColor"/>
         </svg>
-        <h1>Victory Lap</h1>
+        <h1>Capital Factory</h1>
+        {#if nextup}
+            <img src="{nextup}" alt="" class="logo" />
+        {/if}
     </div>
 </div>
 
